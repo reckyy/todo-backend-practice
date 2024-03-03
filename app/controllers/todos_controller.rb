1c9c7e458f16154c542d3837cc512fc27e1b8861
reckyy <srreki98@gmail.com>
2024-03-03 15:37:50 +0900

    ⚡️ feat: create機能

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,4 +4,26 @@ class TodosController < ApplicationController
 
     render json: @todos
   end
+
+  def show
+    @todo = Todo.find(params[:id])
+
+    render json: @todo
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      render json: @todo, status: :created, location: @todo
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title, :content)
+  end
 end
